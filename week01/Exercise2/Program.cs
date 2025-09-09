f89215c4cb5e4e53a98b32880d7fadecd6b53e63
yourusername <jorgesilva161@hotmail.com>
2025-09-09 07:16:22 -0600

    ejercicio 2 corregido

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,75 +4,85 @@ class Program
 {
     static void Main(string[] args)
     {
+        string userGrade;
         Console.Write("What is your grade porcentaje?");
         string grade = Console.ReadLine();
         int gradeInt = int.Parse(grade);
 
-        Console.Write("Your grade is ");
-        
         if (gradeInt >= 90)
-            {
+        {
             if (gradeInt <= 93)
             {
-                Console.WriteLine("A-");
+                userGrade = "A-";
             }
             else
             {
-                Console.WriteLine("A");
-            }           
-         }
+                userGrade = "A";
+            }
+        }
 
         else if (gradeInt >= 80)
         {
             if (gradeInt <= 83)
             {
-                Console.WriteLine("B-");
+                userGrade = "B-";
             }
             else if (gradeInt >= 87)
             {
-                Console.WriteLine("B+");
+                userGrade = "B+";
             }
             else
             {
-                Console.WriteLine("B");
+                userGrade = "B";
             }
-             
+
         }
 
         else if (gradeInt >= 70)
-        {   
+        {
             if (gradeInt <= 73)
             {
-                Console.WriteLine("C-");
+                userGrade = "C-";
             }
             else if (gradeInt >= 77)
             {
-                Console.WriteLine("C+");
+                userGrade = "C+";
             }
             else
             {
-                Console.WriteLine("C");
+                userGrade = "C";
             }
-            
+
         }
         else if (gradeInt >= 60)
         {
             if (gradeInt <= 63)
             {
-                Console.WriteLine("D-");
+                userGrade = "D-";
             }
             else if (gradeInt >= 67)
             {
-                Console.WriteLine("D+");
+                userGrade = "D+";
             }
             else
             {
-                Console.WriteLine("D");
+                userGrade = "D";
             }
         }
         else
         {
-            Console.WriteLine("F");
+            userGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {userGrade} ");
+        
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("You passed the class. Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class. Better luck next time!");
         }
 
 
